08a9eab712cd46e7d73da7b400c08f022d4a1e0f
ologinov <olognv@gmail.com>
2017-10-22 12:21:34 +0300

    1

--- OLOG/BT/LayoutBootstrap4.php
+++ OLOG/BT/LayoutBootstrap4.php
@@ -36,6 +36,13 @@ class LayoutBootstrap4 implements
                     padding-top: 5rem;
                 }
             </style>
+
+            <!-- Bootstrap core JavaScript
+            ================================================== -->
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <!--<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
+            <script src="/assets/bootstrap4/assets/js/vendor/popper.min.js"></script>
+            <script src="/assets/bootstrap4/js/bootstrap.js"></script>
         </head>
 
         <body>
@@ -70,13 +77,6 @@ class LayoutBootstrap4 implements
         </main><!-- /.container -->
 
 
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <!--<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
-        <script src="/assets/bootstrap4/js/bootstrap.js"></script>
-        <script src="/assets/bootstrap4/assets/js/vendor/popper.min.js"></script>
         </body>
         </html>
         <?php
