3692eef5f8409316c8fb7254f91dbcbbc2050d38
ologinov <olognv@gmail.com>
2017-10-23 23:33:07 +0300

    1

--- OLOG/BT/BT.php
+++ OLOG/BT/BT.php
@@ -27,10 +27,10 @@ class BT
 		<div class="modal-dialog" role="document">
 		<div class="modal-content">
 		<div class="modal-header">
+		<h5 class="modal-title">' . $title . '</h5>
 		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 		<span aria-hidden="true">&times;</span>
 		</button>
-		<h4 class="modal-title">' . $title . '</h4>
 		</div>
 		<div class="modal-body">' . $contents_html . '</div>
 		</div>
